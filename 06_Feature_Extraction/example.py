2b028245f2d260eeb3cbf89252bf3e1f4f479805
mdsohailalam0805-collab <mdsohail.alam0805@gmail.com>
2026-08-01 22:00:13 +0530

    add feature extraction notes with examples

--- 06_Feature_Extraction/example.py
+++ 06_Feature_Extraction/example.py
@@ -1,3 +1,5 @@
+
+
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 sentences = [
@@ -38,3 +40,38 @@ print("Vocabulary:", tfidf.get_feature_names_out())
 #  [0.00 0.00 0.72 0.69 0.00]]
 
 # Vocabulary: ['i' 'love' 'interesting' 'nlp' 'python']
+
+
+## feature extraction using CountVectorizer and TfidfVectorizer
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+documents = [
+    "I love programming in Python.",
+    "Python is a great programming language.",
+    "I enjoy learning new programming languages." ]
+
+vectorizer = CountVectorizer(ngram_range=(1, 2))  # Using unigrams and bigrams)
+
+X = vectorizer.fit_transform(documents)
+
+print("vocabulary:", vectorizer.get_feature_names_out())
+print("Bag of Words Matrix:\n", X.toarray())
+
+
+## tf-idf feature extraction using TfidfVectorizer
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+documents = [
+    "I love programming in Python.",
+    "Python is a great programming language.",
+    "I enjoy learning new programming languages."
+]
+
+vectorizer = TfidfVectorizer() #  Using default parameters
+
+X = vectorizer.fit_transform(documents) # just fit and transform the documents
+
+print("vocabulary:", vectorizer.get_feature_names_out()) # build the vocabulary and print it
+print("TF-IDF Matrix:\n", X.toarray())  # print the TF-IDF matrix
